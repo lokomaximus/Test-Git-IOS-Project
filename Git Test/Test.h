17435529f1f6d671cf1cb728c61d6c24a793b8fb
Javier Frank <mac@MacBook-Pro-de-mac.local>
2014-10-14 23:41:01 -0500

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Javier Frank on 14/10/14.
+//  Copyright (c) 2014 Javier Frank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Javier Frank on 14/10/14.
+//  Copyright (c) 2014 Javier Frank. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
